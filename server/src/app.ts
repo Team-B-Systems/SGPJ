b9002c73aba61c07479dc0f873bd09cfa6170438
unknown <novaisdavid.19@gmail.com>
2025-09-11 14:31:22 +0100

    crud da funcionalidade  queixa

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import expressListRoutes from 'express-list-routes'
 import authRouter from './modules/auth/auth.routes';
 import userRouter from "./modules/user/user.routes";
 import adminRouter from "./modules/admin/admin.routes";
+import queixaRouter from "./modules/queixa/queixa.routes";
+import depaRouter from "./modules/departamento/departamento.routes";
+import ppassivaRouter from "./modules/partepassiva/partepassiva.routes";
 
 
 const PORT = process.env.PORT || 3000;
@@ -26,11 +29,23 @@ expressListRoutes(authRouter, { prefix: "/auth", forceUnixPathStyle: true });
 
 app.use("/funcionario", userRouter);
 
+app.use("/queixa", queixaRouter)
+
+expressListRoutes(queixaRouter, { prefix: "/queixa", forceUnixPathStyle: true });
+
 expressListRoutes(userRouter, { prefix: "/funcionario", forceUnixPathStyle: true });
 
 app.use("/admin", adminRouter)
 
 expressListRoutes(adminRouter, { prefix: "/admin", forceUnixPathStyle: true });
+
+app.use("/depart", depaRouter)
+
+expressListRoutes(depaRouter, { prefix: "/depart", forceUnixPathStyle: true });
+
+app.use("/ppassiva", ppassivaRouter)
+
+expressListRoutes(ppassivaRouter, { prefix: "/ppassiva", forceUnixPathStyle: true });
 
 app.listen(PORT, () => {
     console.log(`\nApp executando na porta ${PORT} 🚀🆗✅`)
