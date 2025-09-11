7268e9abec354cf926206c3121d16718f2a8a78c
unknown <novaisdavid.19@gmail.com>
2025-09-11 17:26:40 +0100

    restrição de acesso adicionada ao criar departamento

--- server/src/modules/departamento/departamento.controller.ts
+++ server/src/modules/departamento/departamento.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import * as departamentoService from "./departamento.service";
 import ApiException from "../../common/Exceptions/api.exception";
+import { AuthRequest } from "src/middlewares/auth.middleware";
+import { Role } from "@prisma/client";
 
 
-export const cadastrar = async (req: Request, res: Response) => {
+export const cadastrar = async (req: AuthRequest, res: Response) => {
     try {
+        if (!req.user || req.user.role !== Role.Admin) {
+            return res.status(401).json({ error: "Não autorizado" })
+        }
+
         const result = await departamentoService.cadastrarDepartamento(req.body);
         res.json(result);
     } catch (err: any) {
